4fc784f80ab257e849ed6e858e9e611b9866a51c
yiouliu1998 <yiouliu2333@gmail.com>
2022-11-15 22:45:03 +0800

    renew readme

--- cp2k.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH -p gsc               # Queue
-#SBATCh -N 1            # Node count required for the job
-#SBATCH -n 24           # Number of tasks to be launched
-#SBATCH -J Cu           # Job name
-
-yhrun -p gsc -N 1 /THFS/opt/cp2k/cp2k-7.1-libxc-libinit-libxsmm/exe/Linux-x86-64-intel/cp2k.popt -in Si_bulk8.inp
--- job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH -p gsc       # Queue
-#SBATCh -N 1            # Node count required for the job
-#SBATCH -n 24           # Number of tasks to be launched
-#SBATCH -J Pu           # Job name
-
-export LD_LIBRARY_PATH=/THFS/opt/intel/composer_xe_2013_sp1.3.174/mkl/lib/intel64:/THFS/opt/intel/composer_xe_2013_sp1.3.174/compiler/lib/intel64:$LD_LIBRARY_PATH
-yhrun -N 1 -p gsc -n 24 /THFS/opt/vasp/5.4.4/vasp.5.4.4/bin/vasp_std > ouput
